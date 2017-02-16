05d0b6d30fc87e27e2ec966cbacbfdef5ef445cd
Hidetaka Matsumoto <tanemomi06@gmail.com>
2017-02-16 15:17:47 +0900

    sample page  /  /about_us

--- app/app.rb
+++ app/app.rb
@@ -60,5 +60,13 @@ module MyFirstPadrino
     #     render 'errors/500'
     #   end
     #
+    # Add these routes below to the app file...
+    get "/" do
+      "Hello World!"
+    end
+
+    get :about, :map => '/about_us' do
+      render :haml, "%p This is a sample blog created to demonstrate how Padrino works!"
+    end
   end
 end
